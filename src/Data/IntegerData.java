1bb639921701395aa891782c6b5d05870b09704e
Baille <mathieu.baille@etu.univ-grenoble-alpes.fr>
2017-04-08 13:51:30 +0200

    Ajout des classes de gestion de donnée

--- src/Data/IntegerData.java
+++ src/Data/IntegerData.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public class IntegerData {
 	
+	
+	
 	public static void main(String[] args) throws IOException {
 		System.out.println("Hello world!");
 	}
---
+++ src/Data/ListData.java
@@ -0,0 +1,5 @@
+package Data;
+
+public class ListData extends IntegerData {
+
+}
