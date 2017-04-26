24f74d0dd5d41605753fedfc318c0912ac88e6f1
Seawolf <Seawolf@R2D2>
2017-04-26 09:20:11 +0200

    Javadoc IntegerData

--- src/main/java/data/IntegerData.java
+++ src/main/java/data/IntegerData.java
@@ -6,6 +6,9 @@ public class IntegerData extends AbstractData {
 	
 	ArrayList<Integer> value = new ArrayList<Integer>();
 	
+	/* (non-Javadoc)
+	 * @see data.Data#set(java.lang.String, java.lang.Object)
+	 */
 	@Override
 	public boolean set(String name, Object value) {
 		boolean res = false;
@@ -19,6 +22,11 @@ public class IntegerData extends AbstractData {
 		return res;
 	}
 	
+	/**
+	 * Add a value "value" which will be referenced under the name "name". The name must not be already used.
+	 * @param name
+	 * @param value
+	 */
 	public void set(String name, int value) {
 		if(!isPresent(name))
 		{
@@ -27,12 +35,19 @@ public class IntegerData extends AbstractData {
 		}
 	}
 
+	
+	/* (non-Javadoc)
+	 * @see data.Data#get(java.lang.String)
+	 */
 	@Override
 	public Object get(String name) {
 		if(isPresent(name))return this.value.get(this.name.indexOf(name));
 		else return null;	
 	}
 	
+	/* (non-Javadoc)
+	 * @see data.Data#remove(java.lang.String)
+	 */
 	@Override
 	public boolean remove(String name) {
 		boolean res = false;
@@ -46,6 +61,12 @@ public class IntegerData extends AbstractData {
 		return res;
 	}
 
+	/**
+	 * Increases by 1 the value of the integer linked to the name "name"
+	 * @param name  name linked to the integer to increase
+	 * @return true if the operation went well
+	 * 		   false if the name doesn't exist
+	 */
 	public boolean incr(String name) {
 		boolean res = false;
 		if(isPresent(name))
@@ -58,6 +79,9 @@ public class IntegerData extends AbstractData {
 		return res;
 	}
 
+	/* (non-Javadoc)
+	 * @see data.Data#isValideValue(java.lang.String)
+	 */
 	@Override
 	public boolean isValideValue(String value) {
 		boolean res;
